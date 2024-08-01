c67b94f80a06658bf93dd4907416dfbd363396d4
Abderrazak Mahi <abderrazakmahiii@gmail.com>
2024-08-01 10:20:48 +0100

    navbar

---
+++ components/Button.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+type ButtonProps = {
+  type: "button" | "submit";
+  title: string;
+  icon?: string;
+  variant: "btn_dark_green";
+};
+
+const Button = ({ type, title, icon, variant }: ButtonProps) => {
+  return (
+    <button
+      type={type}
+      className={`flex items-center px-2 py-1 bg-green-600 text-white rounded ${variant}`}
+    >
+      {icon && <Image src={icon} alt={title} width={24} height={24} />}
+      <span className="ml-2 bold-16 whitespace-nowrap">{title}</span>
+    </button>
+  );
+};
+
+export default Button;
---
+++ components/Navbar.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
+import { NAV_LINKS } from "@/constants";
+import Button from "./Button";
+
+const Navbar = () => {
+  return (
+    <nav className="flex justify-between items-center max-w-screen-xl mx-auto px-4 py-5">
+      <Link href="/">
+        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
+      </Link>
+      <ul className="hidden h-full gap-12 lg:flex items-center">
+        {NAV_LINKS.map((link) => (
+          <li key={link.key}>
+            <Link
+              href={link.href}
+              className="regular-16 text-gray-50 flex items-center cursor-pointer pb-1.5 transition-all hover:font-bold"
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <div className="lg:flexCenter hidden">
+        <Button
+          type="button"
+          title="Login"
+          icon="/user.svg"
+          variant="btn_dark_green"
+        />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+``;
