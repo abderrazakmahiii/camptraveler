ce9bacd5c94c43953b3be251bd7ec9e0ac452dfa
Abderrazak Mahi <abderrazakmahiii@gmail.com>
2024-08-05 15:32:45 +0100

    images

--- constants/index.ts
+++ constants/index.ts
@@ -9,38 +9,38 @@ export const NAV_LINKS = [
 
 // CAMP SECTION
 export const PEOPLE_URL = [
-  "/person-1.png",
-  "/person-2.png",
-  "/person-3.png",
-  "/person-4.png",
+  "/camptraveler/person-1.png",
+  "/camptraveler/person-2.png",
+  "/camptraveler/person-3.png",
+  "/camptraveler/person-4.png",
 ];
 
 // FEATURES SECTION
 export const FEATURES = [
   {
     title: "Real maps can be offline",
-    icon: "/map.svg",
+    icon: "/camptraveler/map.svg",
     variant: "green",
     description:
       "We provide a solution for you to be able to use our application when climbing, yes offline maps you can use at any time there is no signal at the location",
   },
   {
     title: "Set an adventure schedule",
-    icon: "/calendar.svg",
+    icon: "/camptraveler/calendar.svg",
     variant: "green",
     description:
       "Schedule an adventure with friends. On holidays, there are many interesting offers from Hilink. That way, there's no more discussion",
   },
   {
     title: "Technology using augment reality",
-    icon: "/tech.svg",
+    icon: "/camptraveler/tech.svg",
     variant: "green",
     description:
       "Technology uses augmented reality as a guide to your hiking trail in the forest to the top of the mountain. Already supported by the latest technology without an internet connection",
   },
   {
     title: "Many new locations every month",
-    icon: "/location.svg",
+    icon: "/camptraveler/location.svg",
     variant: "orange",
     description:
       "Lots of new locations every month, because we have a worldwide community of climbers who share their best experiences with climbing",
@@ -77,10 +77,10 @@ export const FOOTER_CONTACT_INFO = {
 export const SOCIALS = {
   title: "Social",
   links: [
-    "/facebook.svg",
-    "/instagram.svg",
-    "/twitter.svg",
-    "/youtube.svg",
-    "/wordpress.svg",
+    "/camptraveler/facebook.svg",
+    "/camptraveler/instagram.svg",
+    "/camptraveler/twitter.svg",
+    "/camptraveler/youtube.svg",
+    "/camptraveler/wordpress.svg",
   ],
 };
