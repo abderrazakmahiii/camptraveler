ed524ff719f4aa2c2c031ff55e8fedd175cc53e5
Abderrazak Mahi <abderrazakmahiii@gmail.com>
2024-08-05 11:39:05 +0100

    get app

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,16 +5,21 @@ type ButtonProps = {
   title: string;
   icon?: string;
   variant: string;
+  full?: boolean;
 };
 
-const Button = ({ type, title, icon, variant }: ButtonProps) => {
+const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
   return (
     <button
       type={type}
-      className={`flex items-center px-2 py-1 justify-center bg-green-600 text-white rounded ${variant}`}
+      className={`flex items-center px-2 py-1 justify-center bg-green-600 text-white rounded ${variant} ${
+        full && "w-full"
+      }`}
     >
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
-      <span className="ml-2 bold-16 whitespace-nowrap">{title}</span>
+      <span className="ml-2 bold-16 whitespace-nowrap cursor-pointer">
+        {title}
+      </span>
     </button>
   );
 };
