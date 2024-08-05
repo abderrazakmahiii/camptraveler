26835eb44fe6906258903246e6ec064e00e79f30
Abderrazak Mahi <abderrazakmahiii@gmail.com>
2024-08-05 15:26:04 +0100

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  basePath: "/camptraveler", // This should match your GitHub repo name
-  assetPrefix: "/camptraveler", // Ensures assets use the correct URL
-};
+const nextConfig = {};
 
-module.exports = nextConfig;
+export default nextConfig;
