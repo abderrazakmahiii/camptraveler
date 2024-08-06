a35a8be5a1599cc1b09eb297ad8de1594933d684
Abderrazak Mahi <abderrazakmahiii@gmail.com>
2024-08-06 09:20:58 +0100

    test

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,12 +8,7 @@ const Navbar = () => {
   return (
     <nav className="flex justify-between items-center max-w-screen-xl mx-auto px-4 py-5">
       <Link href="/">
-        <Image
-          src="/camptraveler/hilink-logo.svg"
-          alt="logo"
-          width={74}
-          height={29}
-        />
+        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
       </Link>
       <ul className="hidden h-full gap-12 lg:flex items-center">
         {NAV_LINKS.map((link) => (
